9672851aa80c2a3a4e016264a785243cf289deaf
daniyal shaffi <daniyalshaffi1600@gmail.com>
2023-08-16 03:41:39 +0500

    complete style home till login page create

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,13 +31,21 @@ var swiperCategories = new Swiper(".categories__container", {
     },
     
     breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
+      350: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+        992: {
           slidesPerView: 4,
-          spaceBetween: 40,
+          spaceBetween: 24,
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 24,
         },
         1400: {
           slidesPerView: 6,
@@ -57,13 +65,13 @@ var swiperProducts = new Swiper(".new__container", {
   },
   
   breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
       768: {
-        slidesPerView: 4,
-        spaceBetween: 40,
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 24,
       },
       1400: {
         slidesPerView: 4,
